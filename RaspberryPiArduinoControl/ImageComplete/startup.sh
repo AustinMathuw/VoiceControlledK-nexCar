8b8749adee6330a2b9fa4571ad7c85dba95b489a
AustinMathuw <siraustin1234@gmail.com>
2016-08-28 20:51:11 -0400

    Added Image Extras

---
+++ RaspberryPiArduinoControl/ImageComplete/startup.sh
@@ -0,0 +1,6 @@
+#/bin/sh
+echo ""
+echo "Starting Alexa Car..."
+sleep 10
+chmod 777 startup1.sh
+./startup1.sh
---
+++ RaspberryPiArduinoControl/ImageComplete/startup1.sh
@@ -0,0 +1,14 @@
+#/bin/sh
+echo ""
+echo ""
+echo ""
+echo "Created by Austin Wilson (16)"
+echo ""
+echo ""
+sudo wget -q --tries=10 --timeout=20 --spider http://google.com
+
+if [ $? -eq 0 ]; then
+	sudo python /home/pi/Desktop/pubnubControlTest.py
+else
+	echo "Please connect to the internet, then reboot."
+fi
